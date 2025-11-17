35cc23c08a85f056960a40e40601d397600344d2
mrkarimi9425 <mrkarimi9425@gmail.com>
2025-11-17 17:46:01 +0330

    first commit

--- App.tsx
+++ App.tsx
@@ -5,8 +5,15 @@
  * @format
  */
 
+import { getUpdateSource, HotUpdater } from '@hot-updater/react-native';
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -14,7 +21,6 @@ import {
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -42,4 +48,34 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default HotUpdater.wrap({
+  source: getUpdateSource(
+    'https://eebsegcyltuzcmgutyld.supabase.co/functions/v1/update-server',
+
+    {
+      updateStrategy: 'appVersion', // or "fingerprint"
+    },
+  ),
+
+  fallbackComponent: ({ progress, status }) => (
+    <View
+      style={{
+        flex: 1,
+        padding: 20,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      }}
+    >
+      <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>
+        {status === 'UPDATING' ? 'Updating...' : 'Checking for Update...'}
+      </Text>
+      {progress > 0 ? (
+        <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>
+          {Math.round(progress * 100)}%
+        </Text>
+      ) : null}
+    </View>
+  ),
+})(App);
